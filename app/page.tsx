74e9d03cb815ba4beda21cf7961c0cd35480a889
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-09-20 23:40:24 +0100

    feat: move header and footer component to homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,14 @@ import CallToAction from "@/components/home/CallToAction";
 import Hero from "@/components/home/Hero";
 import Portfolio from "@/components/home/Portfolio";
 import Services from "@/components/home/Services";
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <Hero />
       <Services />
       <Portfolio />
       <CallToAction />
-      <Footer />
     </main>
   );
 }
