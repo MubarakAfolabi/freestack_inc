2e69c1ca780c0b5ef12b507e872ff67680fb7863
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-09-21 12:06:29 +0100

    feat: add mobile layout for homepage CTA section

--- components/home/CallToAction.tsx
+++ components/home/CallToAction.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function CallToAction() {
   return (
-    <section className="bg-[hsla(212,80%,42%,1)] text-white flex flex-col gap-8 items-center justify-center p-16 mb-20">
-      <h2 className="text-3xl font-bold">
+    <section className="bg-[hsla(212,80%,42%,1)] text-white flex flex-col gap-8 items-center justify-center p-4 md:p-16 mb-20">
+      <h2 className="text-2xl md:text-3xl font-bold text-center">
         Ready to build something structurally sound?
       </h2>
-      <p>
+      <p className="text-center">
         Let&apos;s turn your concept into a scalable digital product from design
         to deployment.
       </p>
