5ecfe4074c0fcf3711bfc3d6864ecb6bb361a203
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-09-21 11:50:49 +0100

    feat: add mobile layout for homepage services section

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -34,12 +34,12 @@ export default function Services() {
   ];
 
   return (
-    <section className="flex flex-col gap-6 bg-[hsla(210,40%,98%,1)] p-16">
+    <section className="flex flex-col gap-4 md:gap-6 bg-[hsla(210,40%,98%,1)] p-4 md:p-16">
       <div className="flex justify-between items-center">
-        <h2 className="text-3xl font-bold">Our Services</h2>
+        <h2 className="text-2xl md:text-3xl font-bold">Our Services</h2>
         <Link
           href="/#"
-          className="flex items-center gap-2 text-lg text-[hsla(212,80%,42%,1)] hover:underline"
+          className="flex items-center gap-2 md:text-lg text-[hsla(212,80%,42%,1)] hover:underline"
         >
           view full services
           <ArrowRight size={18} strokeWidth={3} />
@@ -55,12 +55,14 @@ export default function Services() {
               key={service.id}
               className="flex gap-4 items-center border border-[hsla(207,90%,61%,1)] py-4 px-2 rounded-xl"
             >
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-[hsla(207,90%,61%,0.1)]">
-                <Icon />
+              <div className="flex items-center justify-center h-8 w-8 md:h-10 md:w-10 rounded-full bg-[hsla(207,90%,61%,0.1)]">
+                <Icon className="size-5 md:size-6" />
               </div>
               <div className="flex flex-col gap-2">
-                <h3 className="text-2xl font-bold">{service.title}</h3>
-                <p className="text-[hsla(215,16%,47%,1)]">
+                <h3 className="text-xl md:text-2xl font-bold">
+                  {service.title}
+                </h3>
+                <p className="text-sm md:text-md text-[hsla(215,16%,47%,1)]">
                   {service.description}
                 </p>
               </div>
