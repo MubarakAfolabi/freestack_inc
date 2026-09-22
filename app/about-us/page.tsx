11be8d8425ee3571609479dd740e8989789ed661
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-09-22 12:34:18 +0100

    feat: add our story component to about-us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,11 +1,13 @@
 import CompanyGoal from "@/components/about-us/CompanyGoal";
 import Hero from "@/components/about-us/Hero";
+import OurStory from "@/components/about-us/OurStory";
 
 export default function AboutUs() {
   return (
     <main>
       <Hero />
       <CompanyGoal />
+      <OurStory />
     </main>
   );
 }
---
+++ components/about-us/OurStory.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function OurStory() {
+  const ourStories = [
+    {
+      title: "Building Modern Organizations",
+      description:
+        "FreeStack Inc. began as a technology and digital solutions company focused on helping organizations build, manage, and improve their digital presence and operations. We realized that modern brands need more than just standard design—they need a unified approach that combines technology, media, creative solutions, and data to solve practical challenges.",
+      img: "/about-us/story-1.jpg",
+    },
+    {
+      title: "Engineering the Sports Ecosystem",
+      description:
+        "This drive for integrated, data-driven solutions led us to our core specialization within the sports industry. We provide specialized solutions designed to help football academies, clubs, and sports organizations operate more professionally, build their digital identity, and leverage performance data. Our approach goes beyond simply managing social media or building websites. We build a complete digital ecosystem that supports an academy’s media, technology, communication, player development, and performance analysis.",
+      img: "/about-us/story-2.jpg",
+    },
+  ];
+
+  return (
+    <section className="flex flex-col gap-6 p-4">
+      <h2 className="text-2xl font-bold">Our Story</h2>
+
+      <ul className="flex flex-col gap-6">
+        {ourStories.map((story, index) => {
+          return (
+            <li key={index} className="flex flex-col gap-4">
+              <h3 className="text-xl font-bold">{story.title}</h3>
+              <p className="text-[hsla(215,16%,47%,1)]">{story.description}</p>
+              <div className="relative w-full h-60 rounded-xl overflow-hidden">
+                <Image src={story.img} alt="" fill className="object-cover" />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
